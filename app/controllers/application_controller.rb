9074db16a6f4fc67e5f344f1fdc5b98c3e095156
ksummerill <kaitlynsummerill@gmail.com>
2020-05-25 18:38:59 -0700

    add signup form, get and post routes for signup in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,20 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
+  get '/' do
     erb :index
+  end
+
+  get '/signup' do
+  # if not logged in, redirect to signup page
+    erb :signup
+  end
+
+  post '/signup' do
+    # check all fields have been filled in
+    # check to see if user exists.
+    # if not, Gardener.create to make a new instance of a Gardener
+    # if so, flash message to tell them an account exists and redirect to login page
   end
 
 end
