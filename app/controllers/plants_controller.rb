ad426f5eb0eba771eab465916571181417f459bd
ksummerill <kaitlynsummerill@gmail.com>
2020-05-29 18:43:47 -0700

    connecting post route for editing a plant

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -64,15 +64,31 @@ class PlantsController < ApplicationController
       flash[:message] = "You must be logged in to view plants"
       redirect '/login'
     else
-      @plant = Plant.find_by(params)
+      @plant = Plant.find_by_id(params[:id])
+      # @plant gives me:  #<Plant:0x00007f83f408b8b0
+       # id: 15,
+       # name: "orange tree",
+       # amount_of_sun: "full sun",
+       # water_frequency: "twice per week",
+       # gardener_id: 2>
+      # binding.pry
       erb :'/plants/edit_plant'
     end
   end
 
-  post '/plants/:id' do
-    # find the plant by the user_id
-    # if the fields aren't empty (are changed), @plant.update with new params
-    # redirect to /plants/@plant.id so gardener can see their edited plant
+  # find the plant by the user_id
+  # if the fields aren't empty (are changed), @plant.update with new params
+  # redirect to /plants/@plant.id so gardener can see their edited plant
+  post '/my_plants/:id' do
+    @plant = Plant.find_by(params[:gardener_id])
+    if !params[:amount_of_sun].empty?
+      @plant.update(amount_of_sun: params[:amount_of_sun])
+      redirect "my_plants/#{@plant.id}"
+    else
+      flash[:message] = "Please make an edit to update"
+      redirect "my_plants/#{@plant.id}"
+    end
+
   end
 
   # as a gardener, i want to be able to delete a plant.
